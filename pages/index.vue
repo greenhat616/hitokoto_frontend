5497858e4e9edf3e359f39419982ada121b4650a
a632079 <a632079@qq.com>
2019-07-09 20:26:49 +0800

    Init

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+      <div>
+        <a-menu v-model="current" mode="horizontal">
+          <a-menu-item key="home">
+            <a-icon type="home" />首页
+          </a-menu-item>
+          <a-menu-item key="api">
+            <a-icon type="api" />接口说明
+          </a-menu-item>
+          <a-menu-item key="about">
+            <a-icon type="global" />接口说明
+          </a-menu-item>
+        </a-menu>
+      </div>
+  </header>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      current: ['mail']
+    }
+  }
+}
+</script>
