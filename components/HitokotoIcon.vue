0d5263d475575d1ed6e6e3ed8d3912e7bacd83c4
haoduor <33804200+haoduor@users.noreply.github.com>
2019-07-11 18:52:23 +0800

    HitokotoIcon（有问题）

---
+++ components/HitokotoIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <hitokoto-icon/>
+</template>
+<style>
+</style>
+<script>
+import HitokotoSvg from 'static/icon.svg'
+
+const HitokotoIcon = {
+  template: `
+    <a-icon :component="HitokotoSvg" />
+  `,
+  data() {
+    return {
+      HitokotoSvg
+    }
+  }
+}
+
+export default {
+  components: {
+    HitokotoIcon
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,67 @@
 <template>
-  <header>
-      <div>
-        <a-menu v-model="current" mode="horizontal">
-          <a-menu-item key="home">
-            <a-icon type="home" />首页
-          </a-menu-item>
-          <a-menu-item key="api">
-            <a-icon type="api" />接口说明
-          </a-menu-item>
-          <a-menu-item key="about">
-            <a-icon type="global" />接口说明
-          </a-menu-item>
-        </a-menu>
-      </div>
-  </header>
+  <a-layout>
+    <a-layout-header>
+      <a-row>
+        <a-col :span="6">
+          <HitokotoIcon></HitokotoIcon> Hitokoto 一言
+        </a-col>
+        <a-col :span="18">
+          <a-menu v-model="current" mode="horizontal">
+            <a-menu-item key="home">
+              <a-icon type="home" />首页
+            </a-menu-item>
+            <a-menu-item key="api">
+              <a-icon type="api" />接口说明
+            </a-menu-item>
+            <a-menu-item key="about">
+              <a-icon type="global" />接口说明
+            </a-menu-item>
+          </a-menu>
+        </a-col>
+        <a-col></a-col>
+      </a-row>
+    </a-layout-header>
+  </a-layout>
+<!-- <header>
+    <div>
+      <a-menu v-model="current" mode="horizontal">
+        <a-menu-item key="home">
+          <a-icon type="home" />首页
+        </a-menu-item>
+        <a-menu-item key="api">
+          <a-icon type="api" />接口说明
+        </a-menu-item>
+        <a-menu-item key="about">
+          <a-icon type="global" />接口说明
+        </a-menu-item>
+      </a-menu>
+    </div>
+  </header> -->
 </template>
+<style>
+.ant-layout-header {
+  background-color: white;
+}
+
+.ant-menu-horizontal {
+  border-bottom: 0px;
+}
+
+.ant-menu-horizontal > .ant-menu-item-active {
+  border-bottom: 0px;
+}
+
+.ant-menu-item-active {
+  border-top: 2px solid #1890ff;
+  border-bottom: 0px;
+}
+</style>
 <script>
+import HitokotoIcon from '../components/HitokotoIcon.vue'
+
 export default {
-  data() {
-    return {
-      current: ['mail']
-    }
+  components: {
+    HitokotoIcon
   }
 }
 </script>
