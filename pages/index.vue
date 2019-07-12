3e0bb46d156ce3f465765a0162e01a68d48f3874
haoduor <33804200+haoduor@users.noreply.github.com>
2019-07-12 22:16:05 +0800

    坏掉的下拉框

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,16 @@
   <a-layout-header>
     <a-row type="flex" align="middle">
       <a-col :span="4">
-        <a-row type="flex" align="middle" justify="left" :gutter="24">
-            <hitokoto-icon :style="{fontSize: '3em'}" />
+        <a-row type="flex" align="middle" justify="start" :gutter="24">
+          <hitokoto-icon :style="{fontSize: '3em'}" />
           <a-col :offset="1">
-            <div class="big-font">Hitokoto</div>
+            <div class="big-font">
+              Hitokoto
+            </div>
           </a-col>
         </a-row>
       </a-col>
-      <a-col :span="8" :offset="6">
+      <a-col :span="7" :offset="9">
         <a-menu mode="horizontal">
           <a-menu-item key="home">
             <a-icon type="home" />首页
@@ -23,9 +25,27 @@
           </a-menu-item>
         </a-menu>
       </a-col>
-      <a-col :span="3" :offset="3">
-        <a-avatar shape="circle" size="large" >GU</a-avatar>
-        <a-button type="primary">登陆</a-button>
+      <a-col :span="4">
+        <a-dropdown>
+          <a-row>
+            <a href="#">
+              <a-avatar shape="circle" size="large">GU</a-avatar>
+              <a href="#">游客</a>
+            </a>
+          </a-row>
+          <a-menu slot="overlay">
+            <a-menu-item key="0">
+              <a href="#">操作1</a>
+            </a-menu-item>
+            <!-- <a-menu-item >
+              <a href="#">操作2</a>
+            </a-menu-item> -->
+            <a-menu-divider />
+            <a-menu-item key="1">
+              <a href="#">登陆</a>
+            </a-menu-item>
+          </a-menu>
+        </a-dropdown>
       </a-col>
     </a-row>
   </a-layout-header>
