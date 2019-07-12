062609ea679aae618752b4f5e8a052777a030cbb
a632079 <a632079@qq.com>
2019-07-12 23:07:06 +0800

    Fix submenu issue

--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,54 @@
 <template>
-<a-layout>
-  <a-layout-header>
-    <a-row type="flex" align="middle">
-      <a-col :span="4">
-        <a-row type="flex" align="middle" justify="start" :gutter="24">
-          <hitokoto-icon :style="{fontSize: '3em'}" />
-          <a-col :offset="1">
-            <div class="big-font">
-              Hitokoto
-            </div>
-          </a-col>
-        </a-row>
-      </a-col>
-      <a-col :span="7" :offset="9">
-        <a-menu mode="horizontal">
-          <a-menu-item key="home">
-            <a-icon type="home" />首页
-          </a-menu-item>
-          <a-menu-item key="api">
-            <a-icon type="api" />接口说明
-          </a-menu-item>
-          <a-menu-item key="about">
-            <a-icon type="global" />接口说明
-          </a-menu-item>
-        </a-menu>
-      </a-col>
-      <a-col :span="4">
-        <a-dropdown>
-          <a-row>
-            <a href="#">
-              <a-avatar shape="circle" size="large">GU</a-avatar>
-              <a href="#">游客</a>
-            </a>
+  <a-layout>
+    <a-layout-header>
+      <a-row type="flex" align="middle">
+        <a-col :span="4">
+          <a-row type="flex" align="middle" justify="start" :gutter="24">
+            <hitokoto-icon :style="{fontSize: '3em'}" />
+            <a-col :offset="1">
+              <div class="big-font">Hitokoto</div>
+            </a-col>
           </a-row>
-          <a-menu slot="overlay">
-            <a-menu-item key="0">
-              <a href="#">操作1</a>
+        </a-col>
+        <a-col :span="11" :offset="9">
+          <a-menu mode="horizontal" v-model="current">
+            <a-menu-item key="home">
+              <a-icon type="home" />首页
             </a-menu-item>
-            <!-- <a-menu-item >
-              <a href="#">操作2</a>
-            </a-menu-item> -->
-            <a-menu-divider />
-            <a-menu-item key="1">
-              <a href="#">登陆</a>
+            <a-menu-item key="api">
+              <a-icon type="api" />接口说明
             </a-menu-item>
+            <a-menu-item key="about">
+              <a-icon type="global" />接口说明
+            </a-menu-item>
+            <a-sub-menu>
+              <span slot="title">
+                <a-avatar shape="circle" size="large">GU</a-avatar> 游客
+                <a-icon type="down" />
+              </span>
+              <a-menu-item key="login">登录</a-menu-item>
+              <a-menu-item key="register">注册</a-menu-item>
+            </a-sub-menu>
           </a-menu>
-        </a-dropdown>
-      </a-col>
-    </a-row>
-  </a-layout-header>
-</a-layout>
+        </a-col>
+      </a-row>
+    </a-layout-header>
+  </a-layout>
 </template>
 <style>
-
 </style>
 <script>
-import HitokotoIcon from '../components/HitokotoIcon.vue'
+import HitokotoIcon from '~/components/HitokotoIcon.vue'
 
 export default {
   props: ['header'],
   components: {
     HitokotoIcon
+  },
+  data() {
+    return {
+      current: ['home']
+    }
   }
 }
 </script>
