49a8cb73d4dff32a3f7ce9a8b6c507d03df19126
a632079 <a632079@qq.com>
2019-07-19 23:57:10 +0800

    修复 header 的样式问题； layout 撑满容器

--- components/commonHeader.vue
+++ components/commonHeader.vue
@@ -1,23 +1,21 @@
-<style>
-  .hitokoto-header .ant-drawer-body {
-    padding: 0;
-  }
-
-</style>
 <template>
-  <div>
+  <div style="background: #ffffff;">
     <a-layout-header class="hitokoto-header">
       <a-row>
         <a-col :xs="{span: 12, offset: 6}" :sm="{span: 6, offset: 9}" :md="{span:4, offset: 0}">
           <div class="logo">
-            <hitokoto-icon :style="{fontSize: '3em'}" />
+            <hitokoto-icon :style="{fontSize: '32px'}" />
             <span class="big-font">Hitokoto</span>
           </div>
         </a-col>
         <a-col :xs="{span:4, offset: 2}" :sm="{span: 4, offset: 5}" :md="{span: 0}">
-          <a-button @click="toggleMenu" style="margin-bottom: 16px">
-            <a-icon type="bars" />
-          </a-button>
+          <a-row type="flex" align="middle" justify="end">
+            <a-col>
+              <a-button class="menu-button" @click="toggleMenu">
+                <a-icon type="menu" />
+              </a-button>
+            </a-col>
+          </a-row>
         </a-col>
         <a-col :xs="{span: 0}" :sm="{span: 0}" :md="{span: 11, offset: 9}">
           <a-menu mode="horizontal" :selected-keys="getCurrent" class='menu-site'>
@@ -31,24 +29,26 @@
               <nuxt-link to="/about"><a-icon type="global" />关于我们</nuxt-link>
             </a-menu-item>
             <a-menu-item key="login">
-              <nuxt-link to="/auth/login"><a-icon type="user" />登录</nuxt-link>
+              <nuxt-link to="/auth/login"><a-icon type="user" />用户登录</nuxt-link>
             </a-menu-item>
           </a-menu>
         </a-col>
       </a-row>
     </a-layout-header>
-    <a-drawer placement="right" :closable="false" @close="onClose" :visible="menu" wrapClassName="hitokoto-header">
+    <a-drawer placement="right" :closable="false" @close="onClose" :visible="menu" wrapClassName="hitokoto-header-drawer">
       <a-menu mode="vertical" :selected-keys="getCurrent">
         <a-menu-item key="home">
-          <a-icon type="home" />首页
+          <nuxt-link to="/"><a-icon type="home" />首页</nuxt-link>
         </a-menu-item>
         <a-menu-item key="api">
           <a-icon type="api" />接口说明
         </a-menu-item>
         <a-menu-item key="about">
-          <a-icon type="global" />接口说明
+          <nuxt-link to="/api"><a-icon type="api" />接口说明</nuxt-link>
         </a-menu-item>
-        <a-menu-item key="login">登录</a-menu-item>
+        <a-menu-item key="login">
+          <nuxt-link to="/auth/login"><a-icon type="user" />用户登录</nuxt-link>
+        </a-menu-item>
       </a-menu>
     </a-drawer>
   </div>
--- components/homeHeader.vue
+++ components/homeHeader.vue
@@ -1,18 +1,18 @@
 <template>
-  <div>
+  <div style="background: #ffffff;">
     <a-layout-header class="home-header">
       <a-row>
         <a-col :xs="{span: 12, offset: 6}" :sm="{span: 6, offset: 9}" :md="{span:4, offset: 0}">
           <div class="logo">
-            <hitokoto-icon :style="{fontSize: '3em'}" />
+            <hitokoto-icon :style="{fontSize: '32px'}" />
             <span class="big-font">Hitokoto</span>
           </div>
         </a-col>
         <a-col :xs="{span:4, offset: 2}" :sm="{span: 4, offset: 5}" :md="{span: 0}">
           <a-row type="flex" align="middle" justify="end">
             <a-col>
-              <a-button style="margin-bottom: 16px" @click="toggleMenu">
-                <a-icon type="bars" />
+              <a-button class="menu-button" @click="toggleMenu">
+                <a-icon type="menu" />
               </a-button>
             </a-col>
           </a-row>
@@ -41,26 +41,34 @@
             </a-menu-item>
             <a-menu-item key="login">
               <nuxt-link to="/auth/login">
-                <a-icon type="user" />登录
+                <a-icon type="user" />用户登录
               </nuxt-link>
             </a-menu-item>
           </a-menu>
         </a-col>
       </a-row>
     </a-layout-header>
-    <a-drawer placement="right" :closable="false" :visible="menu" wrap-class-name="hitokoto-header" @close="onClose">
+    <a-drawer placement="right" :closable="false" :visible="menu" wrap-class-name="hitokoto-header-drawer" @close="onClose">
       <a-menu :selected-keys="getCurrent" mode="vertical">
         <a-menu-item key="home">
-          <a-icon type="home" />首页
+          <nuxt-link to="/">
+            <a-icon type="home" />首页
+          </nuxt-link>
         </a-menu-item>
         <a-menu-item key="api">
-          <a-icon type="api" />接口说明
+          <nuxt-link to="/api">
+            <a-icon type="api" />接口说明
+          </nuxt-link>
         </a-menu-item>
         <a-menu-item key="about">
-          <a-icon type="global" />关于我们
+          <nuxt-link to="/about">
+            <a-icon type="global" />关于我们
+          </nuxt-link>
         </a-menu-item>
         <a-menu-item key="login">
-          登录
+          <nuxt-link to="/auth/login">
+            <a-icon type="user" />用户登录
+          </nuxt-link>
         </a-menu-item>
       </a-menu>
     </a-drawer>
