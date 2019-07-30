df5907718251f120c04fcad3162705864ed0eefd
a632079 <a632079@qq.com>
2019-07-30 12:19:00 +0800

    中文本地优化; 以及一些优化性更新

---
+++ pages/dashboard/user/security.vue
@@ -0,0 +1,24 @@
+<template>
+  <dashboard-layout :menu-selected="menuSelected" :menu-opened="menuOpened">
+    <h1>账户安全</h1>
+  </dashboard-layout>
+</template>
+<script>
+import dashboardLayout from '~/components/dashboardLayout.vue'
+export default {
+  components: {
+    dashboardLayout
+  },
+  data() {
+    return {
+      menuSelected: ['user-security'],
+      menuOpened: ['user']
+    }
+  },
+  head() {
+    return {
+      title: '账户安全'
+    }
+  }
+}
+</script>
